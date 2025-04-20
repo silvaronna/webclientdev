e79c01e20e02a806b8fa1d92a354e2ad0a90ed60
Azka Abdillah <azka.abdillah@outlook.co.id>
2025-04-20 11:32:26 +0700

    fix: replace deprecated @import with @use in global.scss

--- web-scrapping-reactJS/SC/vite.config.js
+++ web-scrapping-reactJS/SC/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import path from "path"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
